e78de315c054a8fff185250718e1eeb2e3d6a153
Tim Moreton <tmoreton@helloinspire.com>
2021-04-12 10:27:11 -0300

    update

--- src/App.js
+++ src/App.js
@@ -12,9 +12,6 @@ class LambdaDemo extends Component {
     e.preventDefault()
 
     this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
   }
 
   render() {
